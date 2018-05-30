59cbc4983457737f897118de955122c221e4c7c3
Victoria Eng <engv@uw.edu>
2018-05-29 20:20:52 -0700

    my changes

--- analysis.R
+++ analysis.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(stringr)
 library(ggplot2)
 library(data.table)
+library(treemap)
 # load in data set
 personalities <- read.csv(file = "BIG5/data.csv", header=TRUE, sep="",
                           stringsAsFactors = F)
@@ -11,41 +12,20 @@ personalities <- read.csv(file = "BIG5/data.csv", header=TRUE, sep="",
 other_info <- personalities[, c("race", "age", "engnat", "gender", "hand", "source", "country")]
 # what is the average age of survey takers?
 under_30 <- nrow(filter(other_info, age < 30))
-# what is the percentage of females?
 
-# plotly big circle on contury lab
 
-install.packages("treemap")
-library(treemap)
-treemap(personalities, #Your data frame object
-        index=c("race","age","country"),  #A list of your categorical variables
-        vSize = "X2017",  #This is your quantitative variable
-        type="index", #Type sets the organization and color scheme of your treemap
-        palette = "Reds",  #Select your color palette from the RColorBrewer presets or make your own.
-        title="Spending in President Obama's 2017 Budget", #Customize your title
-        fontsize.title = 14 #Change the font size of the title
-)
-
+# make dataframe listing unique countries and frequency
 unique_countries <- unique(other_info$country)
 num_each_country <- sapply(unique_countries, class) == 'character'
 value <- group_by(personalities, country) %>% 
   summarise(count=n())
+value[1, 1] = "NA"
             
-  
 
-#data.table 1.8.0  For help type: help("data.table")
-
-by_unique <- personalities[,list(sale=sum(sale), profit=sum(profit)), by=unique_countries]
-
-# Create data
-group= unique_countries
-value=c(13,5,22)
-data=data.frame(group,value)
-
-# treemap
-treemap(data,
-        index="unique_countries",
-        vSize="value",
+# treemap of different countries participants represent
+treemap(value,
+        index="country",
+        vSize="count",
         type="index"
 )
 
